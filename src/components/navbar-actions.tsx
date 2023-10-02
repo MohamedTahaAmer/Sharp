8148a0be5547fc63668a8536e3973aecd9daeeb4
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-10-02 03:06:05 +0300

    zustand back to work, by using a useState and useEffect

--- src/components/navbar-actions.tsx
+++ src/components/navbar-actions.tsx
@@ -2,11 +2,18 @@
 
 import { ShoppingBag } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import useCart from '@/hooks/use-cart';
 
 const NavbarActions = () => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
 	const router = useRouter();
 	const cart = useCart();
 
@@ -18,7 +25,9 @@ const NavbarActions = () => {
 				className='flex h-fit items-center rounded-full bg-foreground px-4 py-2 text-background hover:border hover:border-foreground hover:bg-background hover:text-foreground'
 			>
 				<ShoppingBag className='aspect-square w-4' />
-				<span className='ml-2 text-sm font-medium'>{cart.items.length}</span>
+				<span className='ml-2 text-sm font-medium'>
+					{!isMounted ? 0 : cart.items.length}
+				</span>
 			</Button>
 		</div>
 	);
